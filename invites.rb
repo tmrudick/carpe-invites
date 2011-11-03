96437ac8166dda9d77d1846ec57fae41ea74e22d
Tom Rudick <tmrudick@gmail.com>
2011-11-03 08:46:21 -0700

    Adding settings.yaml

--- invites.rb
+++ invites.rb
@@ -4,3 +4,7 @@ require 'rubygems'
 require 'mail'
 require 'yaml' # Should switch to 1.9.2 and psych
 
+# load settings from settings.yaml
+settings = YAML.load_file('settings.yaml')
+
+puts settings["host"]
